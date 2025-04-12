466cf27fad479195cedacfb589acc4079d6bbcea
TheYoha <42351905+TheYoha@users.noreply.github.com>
2025-04-12 12:51:04 -0400

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 # Supabase setup
 SUPABASE_URL = "https://utcuhckkcgdzwcktvgva.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Y3VoY2trY2dkendja3R2Z3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0Njk2ODMsImV4cCI6MjA2MDA0NTY4M30.J3bYl36RNzNrqFmUq8x1vaww_f_Jegd35dL2ScqKvyk"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 async def log_call(method, path, model, status_code):
